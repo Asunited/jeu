43c45e80111440112baef583dbf22ea16f16ef2b
Asunited <bapt.vautrelle@gmail.com>
2022-03-20 15:11:20 +0100

    final

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,8 +1,15 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
+
 canvas.width = innerWidth
 canvas.height = innerHeight
+
+
+const scoreEl = document.querySelector('#scoreEl')
+const startGameBtn = document.querySelector('#startGameBtn')
+const modalEl = document.querySelector('#modalEl')
+const bigScoreEl = document.querySelector('#bigScoreEl')
 
 class Player {
     constructor(x, y, radius, color) {
@@ -63,14 +70,52 @@ class Enemy {
         this.y = this.y + this.velocity.y
     }
 }
+const friction = 0.99
+class Particle {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+        this.alpha = 1
+    }
+    draw() {
+        c.save()
+        c.globalAlpha = this.alpha
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+        c.restore()
+    }
+    update() {
+        this.draw()
+        this.velocity.x *= friction
+        this.velocity.y *= friction
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+        this.alpha -= 0.01
+    }
+}
 
 const x = canvas.width / 2
 const y = canvas.height / 2
 
-const player = new Player(x, y, 30, 'blue')
+let player = new Player(x, y, 10, 'white')
+let projectiles = []
+let enemies = []
+let particles = []
 
-const projectiles = []
-const enemies = []
+function init() {
+    player = new Player(x, y, 10, 'white')
+    projectiles = []
+    enemies = []
+    particles = []
+    score = 0
+    scoreEl.innerHTML = score
+    bigScoreEl.innerHTML = score
+}
 
 function spawnEnemy() {
     setInterval(() => {
@@ -87,7 +132,8 @@ function spawnEnemy() {
             x = Math.random() * canvas.width
             y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
         }
-        const color = 'green'
+        //error not good color 
+        const color = "hsl(" + Math.random() * 360 + ", 50%, 50%)"
 
         const angle = Math.atan2(
             canvas.height / 2 - y,
@@ -102,12 +148,32 @@ function spawnEnemy() {
 }
 
 let animationId
+let score = 0
 function animate() {
     animationId = requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
+    c.fillStyle = 'rgb(0, 0, 0, 0.1)'
+    c.fillRect(0, 0, canvas.width, canvas.height)
     player.draw()
+    particles.forEach((particle, index) => {
+        if (particle.alpha <= 0) {
+            particles.splice(index, 1)
+        } else {
+            particle.update()
+        }
+
+    });
     projectiles.forEach((projectile) => {
         projectile.update()
+        // remove from edges of screen
+        if (projectile.x + projectile.radius < 0 &&
+            projectile.x - projectile.radius > canvas.width &&
+            projectile.y + projectile.radius < 0 &&
+            projectile.y - projectile.radius > canvas.height) {
+
+            setTimeout(() => {
+                projectiles.splice(index, 1)
+            }, 0)
+        }
     })
     enemies.forEach((enemy, index) => {
         enemy.update()
@@ -116,35 +182,74 @@ function animate() {
 
         // end game
         if (dist - enemy.radius - player.radius < 1) {
-            console.log('end');
             cancelAnimationFrame(animationId)
+            modalEl.style.display = 'flex'
+            bigScoreEl.innerHTML = score
         }
 
 
         projectiles.forEach((projectile, projectileIndex) => {
             const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
-            if (dist - enemy.radius - projectile.radius < 1) {
-                enemies.splice(index, 1)
-                projectiles.splice(projectileIndex, 1)
+            //when projectiles touch ennemy
 
+            if (dist - enemy.radius - projectile.radius < 1) {
+
+
+                //create explosion
+                for (let i = 0; i < 8; i++) {
+                    particles.push(
+                        new Particle(
+                            projectile.x,
+                            projectile.y,
+                            Math.random() * 2,
+                            enemy.color,
+                            {
+                                x: (Math.random() - 0.5) * Math.random() * 6,
+                                y: (Math.random() - 0.5) * Math.random() * 6
+                            }))
+                }
+                if (enemy.radius - 10 > 5) {
+                    //increase our score
+                    score += 100
+                    scoreEl.innerHTML = score
+                    gsap.to(enemy, {
+                        radius: enemy.radius - 10
+                    })
+                    setTimeout(() => {
+                        projectiles.splice(index, 1)
+                    }, 0)
+                } else {
+                    setTimeout(() => {
+                        //remove from scene altogether
+                        score += 250
+                        scoreEl.innerHTML = score
+                        enemies.splice(index, 1)
+                        projectiles.splice(projectileIndex, 1)
+                    }, 0)
+                }
             }
         })
     })
 }
 
 window.addEventListener('click', (event) => {
+
     const angle = Math.atan2(
         event.clientY - canvas.height / 2,
         event.clientX - canvas.width / 2
     )
     const velocity = {
-        x: Math.cos(angle),
-        y: Math.sin(angle)
+        x: Math.cos(angle) * 4,
+        y: Math.sin(angle) * 4
     }
     projectiles.push(
-        new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity)
+        new Projectile(canvas.width / 2, canvas.height / 2, 5, 'white', velocity)
     )
 })
 
-animate()
-spawnEnemy()
+startGameBtn.addEventListener('click', () => {
+    init()
+    animate()
+    spawnEnemy()
+    modalEl.style.display = 'none'
+})
